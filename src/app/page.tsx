cfe7e17661e87e427e96e495b7804fcbc5e93f62
Ann Dolbina <dolbinaann@gmail.com>
2024-02-06 23:39:54 +0200

    add tailwind and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+
+import StatusLabel, {Status} from "@/app/components/status-label";
+import { headers } from "next/headers";
+
 export default function Home() {
+  console.log(headers());
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page{new Date().toTimeString() }</h1>
+      <StatusLabel status={Status.Active} disabled>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive} disabled>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending} disabled>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended} disabled>Suspended</StatusLabel>
     </main>
   );
 }
